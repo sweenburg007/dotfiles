bfb509dff81400d04c81095ba97f38977d3952d3
Stephen Sweeney <stephen.sweeney@jhuapl.edu>
2026-05-09 12:25:58 -0400

    Minor updated to blink.cmp configuration

--- private_dot_config/nvim/lua/plugins/blink.lua
+++ private_dot_config/nvim/lua/plugins/blink.lua
@@ -68,7 +68,8 @@ if vim.fn.exists("g:vscode") ~= 1 then
                 },
             },
             completion = {
-                documentation = { auto_show = false, auto_show_delay_ms = 500 },
+                documentation = { auto_show = true, auto_show_delay_ms = 500 },
+                ghost_text = { enabled = true }
             },
             sources = {
                 default = { 'lsp', 'path', 'snippets', 'buffer' },
